226dce6274233a93c27e7649bf79bff40e16d4af
azure <947612561@qq.com>
2022-11-26 20:58:03 +0800

    hot-fix commit1

--- src/main/java/git/test01.java
+++ src/main/java/git/test01.java
@@ -5,5 +5,7 @@ public class test01 {
         System.out.println("hello git!");
         System.out.println("hello git!!!!!");
         System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
